b3ad4a6a621ee009b5045ed1874aabf143367946
alicecoder96 <alicecoder96@gmail.com>
2023-02-07 01:13:40 +0900

    imageフォルダ追加

--- src/sample.py
+++ src/sample.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+import os
+import sys
 
 chrome_options = webdriver.ChromeOptions()
 driver = webdriver.Remote(
@@ -7,4 +9,16 @@ driver = webdriver.Remote(
 )
 driver.get("http://www.google.com")
 print(driver.title)
+
+FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images/screen.png")
+# get width and height of the page
+w = driver.execute_script("return document.body.scrollWidth;")
+h = driver.execute_script("return document.body.scrollHeight;")
+
+# set window size
+driver.set_window_size(w,h)
+
+# Get Screen Shot
+driver.save_screenshot(FILENAME)
+
 driver.quit()
